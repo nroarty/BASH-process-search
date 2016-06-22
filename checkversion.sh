91d7f19e0bd8014b669cdb1ab17683143fce4a72
nroarty <nathan@njrconsulting.com>
2016-06-21 22:59:28 -0700

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   checkversion.sh - updated variable names
    	modified:   sha1_extract.sh - tidied up to align with standards and improve readability

--- checkversion.sh
+++ checkversion.sh
@@ -2,12 +2,12 @@
 
 # script to check for version number of operating system against test
 
-VerifiedOS="Linux"			# OS verified with script
-VerifiedRelease="4.4.0-24-generic"	# OS version verified with script
+VERIFIED_OS="Linux"			# OS verified with script
+VERIFIED_RELEASE="4.4.0-24-generic"	# OS version verified with script
 
 # Checking versions using specific commands to extract version/os information
 
-if [ "$(uname -s)" = "$VerifiedOS" ] && [ "$(uname -r)" = "$VerifiedRelease" ] ; then
+if [ "$(uname -s)" = "$VERIFIED_OS" ] && [ "$(uname -r)" = "$VERIFIED_RELEASE" ] ; then
 	echo "Operating system and version are verified"
 else
 	echo "WARNING - This script has not been tested against this release"
@@ -16,9 +16,9 @@ fi
 
 # Using Awk to extract fields from generic output for compafison
 
-if [ "$(uname -a | awk '{print $1}')" = "$VerifiedOS" ] \
+if [ "$(uname -a | awk '{print $1}')" = "$VERIFIED_OS" ] \
    && \
-   [ "$(uname -a | awk '{print $3}')" = "$VerifiedRelease" ] ; then
+   [ "$(uname -a | awk '{print $3}')" = "$VERIFIED_RELEASE" ] ; then
 	echo "Operating system and version are verified, using awk to extract"
 else
 	echo "WARNING - This script has not been tested against this release, using awk to extract"
--- sha1_extract.sh
+++ sha1_extract.sh
@@ -1,47 +1,50 @@
 #!/bin/bash
 
-# sha1_extract.sh
-# Version 0.1
+# sha1_extract_3_1.sh
+# Version 0.3
 
-JDKversion=1.8.0_25					# version of JDK tested with this script
-keystore_loc=~/.local/share/Xamarin/Mono\ for\ Android/debug.keystore  # location of keystore
+JDK_VERSION=1.8.0_25					# version of JDK tested with this script
+KEYSTORE_PATH=~/.local/share/Xamarin/Mono\ for\ Android/debug.keystore  # location of keystore
+KEYTOOL_PATH=/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands/keytool # location of keytool
 
 # script exit error codes
-err_keytool=60						# keytool not found on system
-err_keystoreloc=61					# keystore not found in default location
+ERR_KEYTOOL=60						# keytool not found on system
+ERR_KEYSTORELOC=61					# keystore not found in default location
 
 
 # Check to see if installed java version is the same the script was tested against
 
 JDKinstall=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' ) # determine the version of java installed
 
-if [ "$JDKinstall" != "$JDKversion" ] ; then
-	echo Script tested against JDK version - $JDKversion
+if [ "$JDKinstall" != "$JDK_VERSION" ] ; then
+	echo Script tested against JDK version - $JDK_VERSION
 	echo Currently installed version of JDK - $JDKinstall
 	echo Script has not been tested with installed version.
 fi
 
-# Verify that the keytool installed on the system - first check if in PATH then in default location
-if [ -z $(which keytool) ] ; then
+# Verify that the keytool installed on the system - first check if in PATH then in default locatio
+
+keytool_location=$(which keytool)
+if [ -z $keytool_location ] ; then
 	echo keytool is not registered in default path
-	if [ -a "/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands/keytool" ] ; then
-			keytool_loc="/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands/keytool"
+	if [ -a "$KEYTOOL_PATH" ] ; then
+			keytool_location=$KEYTOOL_PATH
 			echo keytool has been found in current java version path
 	else
 		echo keytool utility is not installed, unable to extract encryption key
-		ext 60
+		ext $ERR_KEYTOOL
 	fi	
-else
-	keytool_loc=$(which keytool)
 fi
 
+
 # Verify that keystore is installed in default location, if present pull out the SHA1 key
-if [ -a "$keystore_loc" ] ; then
-	keystore=$($keytool_loc -list -v -keystore "$keystore_loc" -storepass android -keypass android | sed -n "/SHA1:/p")
+
+if [ -a "$KEYSTORE_PATH" ] ; then
+	keystore=$($keytool_location -list -v -keystore "$KEYSTORE_PATH" -storepass android -keypass android | sed -n "/SHA1:/p")
 	echo $keystore
 else
 	echo Keystore not present in default location, unable to extact encryption key
-	exit $err_keystoreloc
+	exit $ERR_KEYSTORELOC
 fi
 
 exit 0
